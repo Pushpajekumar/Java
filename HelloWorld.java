127c2ab680aab7049e0efe41b9973bde14a9b5ad
Pushpajekumar <mk6358522@gmail.com>
2023-02-14 19:44:09 +0530

    Hello world

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// public class helloworld {
+//     public static void main(String[] args) {
+//         System.out.println("This is pushpaje kumar"); 
+//     }
+// }
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
