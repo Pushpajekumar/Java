b9f5f4923542f8ee31a8708698f8916a54c47915
Pushpajekumar <mk6358522@gmail.com>
2023-02-14 20:06:50 +0530

    Variables and taking input

---
+++ variables/Variables.java
@@ -0,0 +1,15 @@
+
+import java.util.*;;
+
+public class Variables {
+   public static void main(String[] args){
+    // String name = "Pushpaje";
+    // int a = 45;
+    // int b = 55;
+    // System.out.println(name);
+    // System.out.println(a+b);
+    Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+    System.out.println(name);
+   }
+}
